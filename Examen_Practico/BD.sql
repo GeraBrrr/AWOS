214201f50b63004d884f1fd76b1ded8ae70fb3ac
elgera <105747965+GeraBrrr@users.noreply.github.com>
2022-09-23 10:24:07 -0500

    Delete Examen_Practico directory

--- Examen_Practico/BD.sql
+++
@@ -1,9 +0,0 @@
-create database CURP;
-create table Curp (
-	nombre varchar(20) not null,
-    primer_apellido varchar(20) not null,
-	segundo_apellido varchar(20) not null,
-    sexo char(2) not null,
-    estado varchar(30) not null,
-    fecha_nac date not null
-);
